99ea66cc902f5b6adf31ae23e2ada1c36b1a6694
Buchi Michelle Okonicha <buchi@Buchis-MacBook-Air.local>
2024-08-23 17:57:10 +0100

    y

--- app/components/TestimonialMarquee.js
+++ app/components/TestimonialMarquee.js
@@ -6,8 +6,8 @@ const TestimonialMarquee = ({ testimonials }) => {
     <div className="min-h-screen flex flex-col items-center w-full overflow-hidden relative transition-all duration-500">
       <div className="flex animate-marquee space-x-8">
         {[...testimonials, ...testimonials].map((testimonial, index) => (
-          <div key={index} className="max-w-[45%] flex-shrink-0 px-4">
-            <div className="flex items-center gap-24 p-6 bg-white shadow-xl rounded-lg">
+          <div key={index} className="md:max-w-[45%] max-w-[25%] flex-shrink-0 px-4">
+            <div className="flex flex-col md:flex-row items-center gap-24 p-6 bg-white shadow-xl rounded-lg">
               <div className="flex-1 bg-[#122455] rounded-lg justify-center items-center m-auto">
                 <Image
                   src={testimonial.image}
@@ -29,24 +29,26 @@ const TestimonialMarquee = ({ testimonials }) => {
         ))}
         {/* Duplicate again to ensure smoothness */}
         {[...testimonials].map((testimonial, index) => (
-          <div key={index + testimonials.length} className="max-w-[45%] flex-shrink-0 px-4">
-            <div className="flex items-center gap-24 p-6 bg-white shadow-xl rounded-lg">
-              <div className="flex-1 bg-[#122455] rounded-lg justify-center items-center m-auto">
-                <Image
-                  src={testimonial.image}
-                  alt={testimonial.name}
-                  width={200}
-                  height={200}
-                  className=" object-cover rounded-full"
-                /> 
-              </div>
-              <div className="flex-1">
-                <p className=" pb-4 text-[#122455] text-lg" >&quot;{testimonial.text}&quot;</p>
-                <p className="font-bold text-lg text-[#122455]">{testimonial.name}</p>
-                <p className="text-[#122455] text-base">{testimonial.role}</p>
-              </div>
+          <div key={index} className="md:max-w-[45%] max-w-[25%] flex-shrink-0 px-4">
+          <div className="flex flex-col md:flex-row items-center gap-24 p-6 bg-white shadow-xl rounded-lg">
+            <div className="flex-1 bg-[#122455] rounded-lg justify-center items-center m-auto">
+              <Image
+                src={testimonial.image}
+                alt={testimonial.name}
+                width={200}
+                height={200}
+                className=" object-cover rounded-full"
+              />
+            </div>
+            <div className="flex-1">
+              <p className="mb-2 text-[#122455] text-lg">
+                &quot;{testimonial.text}&quot;
+              </p>
+              <p className="font-bold text-lg text-[#122455]s">{testimonial.name}</p>
+              <p className="text-[#122455] text-base">{testimonial.role}</p>
             </div>
           </div>
+        </div>
         ))}
       </div>
     </div>
