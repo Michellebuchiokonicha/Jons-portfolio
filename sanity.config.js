e728f129ed9ed6dcfa34649ad979cc9ee52e5004
Buchi Michelle Okonicha <buchi@Buchis-MacBook-Air.local>
2024-09-27 07:53:58 +0100

    u

--- sanity.config.js
+++ sanity.config.js
@@ -1,9 +1,5 @@
 'use client'
 
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.jsx` route
- */
-
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
