4ea0c6e94e4e6274cf6a2b7e33c3c7368a745778
Buchi Michelle Okonicha <buchi@Buchis-MacBook-Air.local>
2024-12-08 16:39:34 +0100

    u

--- app/components/Modal.js
+++ app/components/Modal.js
@@ -1,31 +1,30 @@
 import React, { useEffect } from "react";
 
 const Modal = ({ children, onClose }) => {
-  const handleKeyDown = (event) => {
-    if (event.key === "Escape") {
-      onClose();
-    }
-  };
-
-  const handleOutsideClick = (event) => {
-    if (event.target.classList.contains("modal-overlay")) {
-      onClose();
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("keydown", handleKeyDown);
-    return () => {
-      document.removeEventListener("keydown", handleKeyDown);
+    useEffect(() => {
+      const handleKeyDown = (event) => {
+        if (event.key === "Escape") {
+          onClose();
+        }
+      };
+  
+      document.addEventListener("keydown", handleKeyDown);
+      return () => {
+        document.removeEventListener("keydown", handleKeyDown);
+      };
+    }, [onClose]); // Add `onClose` as a dependency to ensure it works with updates
+  
+    const handleOutsideClick = (event) => {
+      if (event.target.classList.contains("modal-overlay")) {
+        onClose();
+      }
     };
-  }, []);
-
   return (
     <div
       className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 modal-overlay"
       onClick={handleOutsideClick}
     >
-      <div className="bg-white p-6 rounded-md shadow-lg w-11/12 lg:w-1/2 relative">
+      <div className="bg-white p-6 rounded-md shadow-lg w-11/12 lg:w-1/2 relative max-h-[80vh] overflow-y-auto">
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
